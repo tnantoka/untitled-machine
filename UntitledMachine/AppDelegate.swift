711af9af910ff20b46e900b828b5137bf863bf76
tnantoka <tnantoka@bornneet.com>
2026-06-11 21:09:54 +0900

    feat: create project

---
+++ UntitledMachine/AppDelegate.swift
@@ -0,0 +1,30 @@
+//
+//  AppDelegate.swift
+//  UntitledMachine
+//
+//  Created by Tatsuya Tobioka on 2026/06/11.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
+        return true
+    }
+
+
+}
+
---
+++ UntitledMachine/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  UntitledMachine
+//
+//  Created by Tatsuya Tobioka on 2026/06/11.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
